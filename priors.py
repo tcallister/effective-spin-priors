9ba18fad06edb225f87483b0026f5c24d0164709
Thomas Callister <tcallister@c02z60enlvdp.cable.rcn.com>
2021-03-17 15:58:20 -0400

    Adding initial version of python script and demo notebook

---
+++ priors.py
@@ -0,0 +1,155 @@
+import numpy as np
+from scipy.special import spence as PL
+
+def Di(z):
+    return PL(1.-z+0j)
+
+def chi_effective_prior_from_aligned_spins(q,aMax,xs):
+
+    # Ensure that `xs` is an array and take absolute value
+    xs = np.reshape(xs,-1)
+
+    # Set up various piecewise cases
+    pdfs = np.zeros(xs.size)
+    caseA = (xs>aMax*(1.-q)/(1.+q))*(xs<=aMax)
+    caseB = (xs<-aMax*(1.-q)/(1.+q))*(xs>=-aMax)
+    caseC = (xs>=-aMax*(1.-q)/(1.+q))*(xs<=aMax*(1.-q)/(1.+q))
+
+    # Select relevant effective spins
+    x_A = xs[caseA]
+    x_B = xs[caseB]
+    x_C = xs[caseC]
+
+    pdfs[caseA] = (1.+q)**2.*(aMax-x_A)/(4.*q*aMax**2)
+    pdfs[caseB] = (1.+q)**2.*(aMax+x_B)/(4.*q*aMax**2)
+    pdfs[caseC] = (1.+q)/(2.*aMax)
+
+    return pdfs
+
+def chi_effective_prior_from_isotropic_spins(q,aMax,xs):
+
+    # Ensure that `xs` is an array and take absolute value
+    xs = np.reshape(np.abs(xs),-1)
+
+    # Set up various piecewise cases
+    pdfs = np.zeros(xs.size,dtype=complex)
+    caseA = (xs<aMax*(1.-q)/(1.+q))*(xs<q*aMax/(1.+q))
+    caseB = (xs<aMax*(1.-q)/(1.+q))*(xs>=q*aMax/(1.+q))
+    caseC = (xs>=aMax*(1.-q)/(1.+q))*(xs<q*aMax/(1.+q))
+    caseD = (xs>=aMax*(1.-q)/(1.+q))*(xs<aMax/(1.+q))*(xs>=q*aMax/(1.+q))
+    caseE = (xs>=aMax*(1.-q)/(1.+q))*(xs>=aMax/(1.+q))*(xs<aMax)
+    caseF = (xs>=aMax)
+
+    # Select relevant effective spins
+    x_A = xs[caseA]
+    x_B = xs[caseB]
+    x_C = xs[caseC]
+    x_D = xs[caseD]
+    x_E = xs[caseE]
+
+    pdfs[caseA] = (1.+q)/(4.*q*aMax**2)*(
+                    q*aMax*(4.+2.*np.log(aMax) - np.log(q**2*aMax**2 - (1.+q)**2*x_A**2))
+                    - 2.*(1.+q)*x_A*np.arctanh((1.+q)*x_A/(q*aMax))
+                    + (1.+q)*x_A*(Di(-q*aMax/((1.+q)*x_A)) - Di(q*aMax/((1.+q)*x_A)))
+                    )
+
+    pdfs[caseB] = (1.+q)/(4.*q*aMax**2)*(
+                    4.*q*aMax
+                    + 2.*q*aMax*np.log(aMax)
+                    - 2.*(1.+q)*x_B*np.arctanh(q*aMax/((1.+q)*x_B))
+                    - q*aMax*np.log((1.+q)**2*x_B**2 - q**2*aMax**2)
+                    + (1.+q)*x_B*(Di(-q*aMax/((1.+q)*x_B)) - Di(q*aMax/((1.+q)*x_B)))
+                    )
+
+    pdfs[caseC] = (1.+q)/(4.*q*aMax**2)*(
+                    2.*(1.+q)*(aMax-x_C)
+                    - (1.+q)*x_C*np.log(aMax)**2.
+                    + (aMax + (1.+q)*x_C*np.log((1.+q)*x_C))*np.log(q*aMax/(aMax-(1.+q)*x_C))
+                    - (1.+q)*x_C*np.log(aMax)*(2. + np.log(q) - np.log(aMax-(1.+q)*x_C))
+                    + q*aMax*np.log(aMax/(q*aMax-(1.+q)*x_C))
+                    + (1.+q)*x_C*np.log((aMax-(1.+q)*x_C)*(q*aMax-(1.+q)*x_C)/q)
+                    + (1.+q)*x_C*(Di(1.-aMax/((1.+q)*x_C)) - Di(q*aMax/((1.+q)*x_C)))
+                    )
+
+    pdfs[caseD] = (1.+q)/(4.*q*aMax**2)*(
+                    -x_D*np.log(aMax)**2
+                    + 2.*(1.+q)*(aMax-x_D)
+                    + q*aMax*np.log(aMax/((1.+q)*x_D-q*aMax))
+                    + aMax*np.log(q*aMax/(aMax-(1.+q)*x_D))
+                    - x_D*np.log(aMax)*(2.*(1.+q) - np.log((1.+q)*x_D) - q*np.log((1.+q)*x_D/aMax))
+                    + (1.+q)*x_D*np.log((-q*aMax+(1.+q)*x_D)*(aMax-(1.+q)*x_D)/q)
+                    + (1.+q)*x_D*np.log(aMax/((1.+q)*x_D))*np.log((aMax-(1.+q)*x_D)/q)
+                    + (1.+q)*x_D*(Di(1.-aMax/((1.+q)*x_D)) - Di(q*aMax/((1.+q)*x_D)))
+                    )
+
+    pdfs[caseE] = (1.+q)/(4.*q*aMax**2)*(
+                    2.*(1.+q)*(aMax-x_E)
+                    - (1.+q)*x_E*np.log(aMax)**2
+                    + np.log(aMax)*(
+                        aMax
+                        -2.*(1.+q)*x_E
+                        -(1.+q)*x_E*np.log(q/((1.+q)*x_E-aMax))
+                        )
+                    - aMax*np.log(((1.+q)*x_E-aMax)/q)
+                    + (1.+q)*x_E*np.log(((1.+q)*x_E-aMax)*((1.+q)*x_E-q*aMax)/q)
+                    + (1.+q)*x_E*np.log((1.+q)*x_E)*np.log(q*aMax/((1.+q)*x_E-aMax))
+                    - q*aMax*np.log(((1.+q)*x_E-q*aMax)/aMax)
+                    + (1.+q)*x_E*(Di(1.-aMax/((1.+q)*x_E)) - Di(q*aMax/((1.+q)*x_E)))
+                    )
+
+    test1 = (
+                2.*(1.+q)*(aMax-x_E)
+                - (1.+q)*x_E*np.log(aMax)**2
+                + np.log(aMax)*(
+                    aMax
+                    -2.*(1.+q)*x_E
+                    -(1.+q)*x_E*np.log(q/((1.+q)*x_E-aMax))
+                    )
+            )
+
+    test2 = (-aMax*np.log(((1.+q)*x_E-aMax)/q)
+                + (1.+q)*x_E*np.log(((1.+q)*x_E-aMax)*((1.+q)*x_E-q*aMax)/q)
+                + (1.+q)*x_E*np.log((1.+q)*x_E)*np.log(q*aMax/((1.+q)*x_E-aMax))
+                - q*aMax*np.log(((1.+q)*x_E-q*aMax)/aMax)
+                + (1.+q)*x_E*(Di(1.-aMax/((1.+q)*x_E)) - Di(q*aMax/((1.+q)*x_E)))
+            )
+
+    pdfs[caseF] = 0.
+
+    return np.real(pdfs)
+
+def chi_p_prior_from_isotropic_spins(q,aMax,xs):
+
+    # Ensure that `xs` is an array and take absolute value
+    xs = np.reshape(xs,-1)
+
+    # Set up various piecewise cases
+    pdfs = np.zeros(xs.size)
+    caseA = xs<q*aMax*(3.+4.*q)/(4.+3.*q)
+    caseB = (xs>q*aMax*(3.+4.*q)/(4.+3.*q))*(xs<aMax)
+
+    # Select relevant effective spins
+    x_A = xs[caseA]
+    x_B = xs[caseB]
+
+    pdfs[caseA] = (1./(aMax**2*q))*((4.+3.*q)/(3.+4.*q))*(
+                    np.arccos((4.+3.*q)*x_A/((3.+4.*q)*q*aMax))*(
+                        aMax
+                        - np.sqrt(aMax**2-x_A**2)
+                        + x_A*np.arccos(x_A/aMax)
+                        )
+                    + np.arccos(x_A/aMax)*(
+                        aMax*q*(3.+4.*q)/(4.+3.*q)
+                        - np.sqrt(aMax**2*q**2*((3.+4.*q)/(4.+3.*q))**2 - x_A**2)
+                        + x_A*np.arccos((4.+3.*q)*x_A/((3.+4.*q)*aMax*q))
+                        )
+                    )
+                    
+    pdfs[caseB] = (1./aMax)*np.arccos(x_B/aMax)
+
+    return pdfs
+
+
+
+
+

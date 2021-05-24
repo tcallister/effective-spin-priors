cf55dd84f16b27425ef94192b8281b64e065cb7d
Tom Callister <thomas.a.callister@gmail.com>
2021-05-24 16:08:08 -0400

    Adding code to compute joint priors on chi_eff and chi_p, with demo notebook

--- priors.py
+++ priors.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.stats import gaussian_kde
 from scipy.special import spence as PL
 
 def Di(z):
@@ -193,3 +194,114 @@ def chi_p_prior_from_isotropic_spins(q,aMax,xs):
 
     return pdfs
 
+def joint_prior_from_isotropic_spins(q,aMax,xeffs,xps,ndraws=10000,bw_method='scott'):
+
+    """
+    Function to calculate the conditional priors p(xp|xeff,q) on a set of {xp,xeff,q} posterior samples.
+
+    INPUTS
+    q: Mass ratio
+    aMax: Maximimum spin magnitude considered
+    xeffs: Effective inspiral spin samples
+    xps: Effective precessing spin values
+    ndraws: Number of draws from the component spin priors used in numerically building interpolant
+
+    RETURNS
+    p_chi_p: Array of priors on xp, conditioned on given effective inspiral spins and mass ratios
+    """
+
+    # Convert to arrays for safety
+    xeffs = np.reshape(xeffs,-1)
+    xps = np.reshape(xps,-1)
+    
+    # Compute marginal prior on xeff, conditional prior on xp, and multiply to get joint prior!
+    p_chi_eff = chi_effective_prior_from_isotropic_spins(q,aMax,xeffs)
+    p_chi_p_given_chi_eff = np.array([chi_p_prior_given_chi_eff_q(q,aMax,xeffs[i],xps[i],ndraws,bw_method) for i in range(len(xeffs))])
+    joint_p_chi_p_chi_eff = p_chi_eff*p_chi_p_given_chi_eff
+
+    return joint_p_chi_p_chi_eff
+
+def chi_p_prior_given_chi_eff_q(q,aMax,xeff,xp,ndraws=10000,bw_method='scott'):
+
+    """
+    Function to calculate the conditional prior p(xp|xeff,q) on a single {xp,xeff,q} posterior sample.
+    Called by `joint_prior_from_isotropic_spins`.
+
+    INPUTS
+    q: Single posterior mass ratio sample
+    aMax: Maximimum spin magnitude considered
+    xeff: Single effective inspiral spin sample
+    xp: Single effective precessing spin value
+    ndraws: Number of draws from the component spin priors used in numerically building interpolant
+
+    RETURNS
+    p_chi_p: Prior on xp, conditioned on given effective inspiral spin and mass ratio
+    """
+
+    # Draw random spin magnitudes.
+    # Note that, given a fixed chi_eff, a1 can be no larger than (1+q)*chi_eff,
+    # and a2 can be no larger than (1+q)*chi_eff/q
+    a1 = np.random.random(ndraws)*aMax
+    a2 = np.random.random(ndraws)*aMax
+
+    # Draw random tilts for spin 2
+    cost2 = 2.*np.random.random(ndraws)-1.
+
+    # Finally, given our conditional value for chi_eff, we can solve for cost1
+    # Note, though, that we still must require that the implied value of cost1 be *physical*
+    cost1 = (xeff*(1.+q) - q*a2*cost2)/a1  
+
+    # While any cost1 values remain unphysical, redraw a1, a2, and cost2, and recompute
+    # Repeat as necessary
+    while np.any(cost1<-1) or np.any(cost1>1):   
+        to_replace = np.where((cost1<-1) | (cost1>1))[0]   
+        a1[to_replace] = np.random.random(to_replace.size)*aMax
+        a2[to_replace] = np.random.random(to_replace.size)*aMax
+        cost2[to_replace] = 2.*np.random.random(to_replace.size)-1.    
+        cost1 = (xeff*(1.+q) - q*a2*cost2)/a1   
+            
+    # Compute precessing spins and corresponding weights, build KDE
+    # See `Joint-ChiEff-ChiP-Prior.ipynb` for a discussion of these weights
+    Xp_draws = chi_p_from_components(a1,a2,cost1,cost2,q)
+    jacobian_weights = (1.+q)/a1
+    prior_kde = gaussian_kde(Xp_draws,weights=jacobian_weights,bw_method=bw_method)
+
+    # Compute maximum chi_p
+    if (1.+q)*np.abs(xeff)/q<aMax:
+        max_Xp = aMax
+    else:
+        max_Xp = np.sqrt(aMax**2 - ((1.+q)*np.abs(xeff)-q)**2.)
+
+    # Set up a grid slightly inside (0,max chi_p) and evaluate KDE
+    reference_grid = np.linspace(0.05*max_Xp,0.95*max_Xp,30)
+    reference_vals = prior_kde(reference_grid)
+
+    # Manually prepend/append zeros at the boundaries
+    reference_grid = np.concatenate([[0],reference_grid,[max_Xp]])
+    reference_vals = np.concatenate([[0],reference_vals,[0]])
+    norm_constant = np.trapz(reference_vals,reference_grid)
+
+    # Interpolate!
+    p_chi_p = np.interp(xp,reference_grid,reference_vals)
+    return p_chi_p
+
+def chi_p_from_components(a1,a2,cost1,cost2,q):
+
+    """
+    Helper function to define effective precessing spin parameter from component spins
+
+    INPUTS
+    a1: Primary dimensionless spin magnitude
+    a2: Secondary's spin magnitude
+    cost1: Cosine of the primary's spin-orbit tilt angle
+    cost2: Cosine of the secondary's spin-orbit tilt
+    q: Mass ratio
+
+    RETRUNS
+    chi_p: Corresponding precessing spin value
+    """
+
+    sint1 = np.sqrt(1.-cost1**2)
+    sint2 = np.sqrt(1.-cost2**2)
+    
+    return np.maximum(a1*sint1,((3.+4.*q)/(4.+3.*q))*q*a2*sint2)
